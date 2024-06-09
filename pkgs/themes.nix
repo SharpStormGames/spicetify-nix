c75bca573734b286e1a4b3b68bd4278a1192c88b
SharpStormGames <112800647+SharpStormGames@users.noreply.github.com>
2024-06-09 14:00:21 +1000

    Remove Almost All Stock Themes

--- pkgs/themes.nix
+++ pkgs/themes.nix
@@ -16,15 +16,6 @@ with source; let
   };
 in {
   official = let
-    dribbblishExt = {
-      filename = "dribbblish.js";
-      src = "${officialThemes}/Dribbblish";
-    };
-
-    turntableExt = {
-      filename = "turntable.js";
-      src = "${officialThemes}/Turntable";
-    };
     mkOfficialTheme = themeName: {
       ${themeName} = {
         name = themeName;
@@ -38,49 +29,6 @@ in {
     };
   in
     {
-      Dribbblish = {
-        name = "Dribbblish";
-        src = officialThemes;
-        requiredExtensions = [dribbblishExt];
-        patches = {
-          "xpui.js_find_8008" = ",(\\w+=)32";
-          "xpui.js_repl_8008" = ",$\{1}56";
-        };
-        injectCss = true;
-        replaceColors = true;
-        overwriteAssets = true;
-        appendName = true;
-        sidebarConfig = true;
-        additionalCss = ''
-          .Root {
-            padding-top: 0px;
-          }
-        '';
-      };
-      text = {
-        name = "text";
-        src = officialThemes;
-        patches = {
-          "xpui.js_find_8008" = ",(\\w+=)56";
-          "xpui.js_repl_8008" = ",$\{1}32";
-        };
-        injectCss = true;
-        replaceColors = true;
-        appendName = true;
-        overwriteAssets = false;
-        sidebarConfig = false;
-      };
-      Dreary = {
-        name = "Dreary";
-        src = officialThemes;
-        sidebarConfig = true;
-        appendName = true;
-      };
-      Turntable = {
-        name = "Turntable";
-        src = officialThemes;
-        requiredExtensions = ["fullAppDisplay.js" turntableExt];
-      };
       catppuccin = {
         name = "catppuccin";
         src = catppuccinSrc;
@@ -90,17 +38,7 @@ in {
         overwriteAssets = true;
         sidebarConfig = false;
       };
-    }
-    // mkOfficialTheme "Ziro"
-    // mkOfficialTheme "Sleek"
-    // mkOfficialTheme "Onepunch"
-    // mkOfficialTheme "Flow"
-    // mkOfficialTheme "Default"
-    // mkOfficialTheme "BurntSienna"
-    // mkOfficialTheme "Blossom"
-    // mkOfficialTheme "Sleek"
-    // mkOfficialTheme "Nightlight"
-    // mkOfficialTheme "Matte";
+    };
   inherit
    Orchis
    ;
